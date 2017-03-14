4bdd0bc7b10f93e9da004367575752ef4b731d71
MuhammadMuneeb <haqgreat@gmail.com>
2017-03-15 04:31:26 +0500

    Created user, order, product migrations and models

---
+++ database/migrations/2017_03_14_231512_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('Product_name', 255);
+            $table->string('Product_Code', 255);
+            $table->integer('Price')->unsigned();
+            $table->string('Description', 255);
+            $table->string("Additional_Notes", 255);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
